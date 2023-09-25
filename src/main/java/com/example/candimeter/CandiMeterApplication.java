911b9f4d72b5bd412425f79ca9c1271d67b7a463
RaniaMohamedHesham <raniaheshamfci40@gmail.com>
2023-09-26 02:02:49 +0300

    first_task

---
+++ src/main/java/com/example/candimeter/CandiMeterApplication.java
@@ -0,0 +1,13 @@
+package com.example.candimeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandiMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandiMeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/candimeter/CandiMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.candimeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandiMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
